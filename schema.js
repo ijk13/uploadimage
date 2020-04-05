c4e7b3437d91d7d3e74fac6e9e3c1b28ce85f816
ijk13 <40337918+ijk13@users.noreply.github.com>
2020-04-05 18:34:03 +0530

    Add files via upload

---
+++ schema.js
@@ -0,0 +1,7 @@
+  
+  
+let mongoose  = require("./config");
+mongoose = mongoose.mongoose
+const nstoreSchema = new mongoose.Schema({brand: String, product:String,price:String,unit:String,productImage:String});
+const nstore = mongoose.model('store', nstoreSchema);
+module.exports.nstore = nstore;
