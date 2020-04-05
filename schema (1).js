730366253baca0728d1916e2fb2286cf70af7cb6
ijk13 <40337918+ijk13@users.noreply.github.com>
2020-04-05 18:33:27 +0530

    Delete schema (1).js

--- schema (1).js
+++
@@ -1,6 +0,0 @@
-  
-let mongoose  = require("./config (1)");
-mongoose = mongoose.mongoose
-const nstoreSchema = new mongoose.Schema({brand: String, product:String,price:String,unit:String,productImage:String});
-const nstore = mongoose.model('store', nstoreSchema);
-module.exports.nstore = nstore;
